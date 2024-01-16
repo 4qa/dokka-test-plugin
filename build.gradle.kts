f8ff8061ff3779fd71731e8e7d1c948f064568f6
Andrey Tyrin <tyrin.andrew@gmail.com>
2024-01-16 16:28:10 +0100

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,105 @@
+plugins {
+    kotlin("jvm") version "1.9.10"
+    id("org.jetbrains.dokka") version "1.9.10" // Used to create a javadoc jar
+    `maven-publish`
+    signing
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+val dokkaVersion: String by project
+dependencies {
+    compileOnly("org.jetbrains.dokka:dokka-core:$dokkaVersion")
+    implementation("org.jetbrains.dokka:dokka-base:$dokkaVersion")
+
+    testImplementation(kotlin("test"))
+    testImplementation("org.jetbrains.dokka:dokka-test-api:$dokkaVersion")
+    testImplementation("org.jetbrains.dokka:dokka-base-test-utils:$dokkaVersion")
+}
+
+kotlin {
+    jvmToolchain(8)
+}
+
+tasks.dokkaHtml {
+    outputDirectory.set(layout.buildDirectory.dir("dokka"))
+}
+
+val javadocJar by tasks.registering(Jar::class) {
+    archiveClassifier.set("javadoc")
+    from(tasks.dokkaHtml)
+}
+
+java {
+    withSourcesJar()
+}
+
+publishing {
+    publications {
+        val dokkaTemplatePlugin by creating(MavenPublication::class) {
+            artifactId = project.name
+            from(components["java"])
+            artifact(javadocJar)
+
+            pom {
+                name.set("Dokka template plugin")
+                description.set("This is a plugin template for Dokka")
+                url.set("https://github.com/Kotlin/dokka-plugin-template/")
+
+                licenses {
+                    license {
+                        name.set("The Apache Software License, Version 2.0")
+                        url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+                        distribution.set("repo")
+                    }
+                }
+
+                developers {
+                    developer {
+                        id.set("JetBrains")
+                        name.set("JetBrains Team")
+                        organization.set("JetBrains")
+                        organizationUrl.set("https://www.jetbrains.com")
+                    }
+                }
+
+                scm {
+                    connection.set("scm:git:git://github.com/Kotlin/dokka-plugin-template.git")
+                    url.set("https://github.com/Kotlin/dokka-plugin-template/tree/master")
+                }
+            }
+        }
+        signPublicationsIfKeyPresent(dokkaTemplatePlugin)
+    }
+
+    repositories {
+        maven("https://oss.sonatype.org/service/local/staging/deploy/maven2/") {
+            credentials {
+                username = System.getenv("SONATYPE_USER")
+                password = System.getenv("SONATYPE_PASSWORD")
+            }
+        }
+    }
+}
+
+fun Project.signPublicationsIfKeyPresent(publication: MavenPublication) {
+    val signingKeyId: String? = System.getenv("SIGN_KEY_ID")
+    val signingKey: String? = System.getenv("SIGN_KEY")
+    val signingKeyPassphrase: String? = System.getenv("SIGN_KEY_PASSPHRASE")
+
+    if (!signingKey.isNullOrBlank()) {
+        extensions.configure<SigningExtension>("signing") {
+            if (signingKeyId?.isNotBlank() == true) {
+                useInMemoryPgpKeys(signingKeyId, signingKey, signingKeyPassphrase)
+            } else {
+                useInMemoryPgpKeys(signingKey, signingKeyPassphrase)
+            }
+            sign(publication)
+        }
+    }
+}
